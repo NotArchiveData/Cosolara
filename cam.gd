a18322e8ecea58b7299fef1939f702ab519595fa
Dhimaan prasad <89119442+NotArchiveData@users.noreply.github.com>
2024-07-06 00:57:02 +0530

    please upload

---
+++ cam.gd
@@ -0,0 +1,20 @@
+extends Camera3D
+
+var move_speed = 30.0
+
+func _process(delta):
+
+	var direction = Vector3.ZERO
+
+	if Input.is_action_pressed("down"):
+		direction -= transform.basis.y.normalized()
+	if Input.is_action_pressed("up"):
+		direction += transform.basis.y.normalized()
+	if Input.is_action_pressed("left"):
+		direction -= transform.basis.x.normalized()
+	if Input.is_action_pressed("right"):
+		direction += transform.basis.x.normalized()
+
+	var new_position = global_transform.origin + direction * move_speed * delta
+	global_transform.origin.x = new_position.x
+	global_transform.origin.z = new_position.z
