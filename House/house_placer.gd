626b43927dd97b1d2edce5ab8b76bd65fc908c43
Dhimaan prasad <89119442+NotArchiveData@users.noreply.github.com>
2024-07-14 22:07:15 +0530

    m

--- House/house_placer.gd
+++ House/house_placer.gd
@@ -7,15 +7,10 @@ const HOUSE = preload("res://House/house.tscn")
 func _on_area_3d_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
-			print("pressed left click")
-			
-			# Instance the house scene
-			var house_instance = HOUSE.instantiate()
-			# Optionally, set the position of the new house instance
-			house_instance.global_transform = self.global_transform
-			# Add the new house instance to the placed_houses node
-			placed_houses.add_child(house_instance)
+			build_my_house()
 			self.queue_free()
 
-func placed_house():
-	pass
+func build_my_house():
+	var house_instantiated = HOUSE.instantiate()
+	house_instantiated.global_transform = self.global_transform
+	placed_houses.add_child(house_instantiated)
