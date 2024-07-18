213ce44dfddc031643e852794f15aea912c9c25f
Dhimaan prasad <89119442+NotArchiveData@users.noreply.github.com>
2024-07-19 00:53:46 +0530

    a little terrain work and free cam!

--- misc/gd/cam.gd
+++ misc/gd/cam.gd
@@ -1,21 +1,55 @@
 extends Camera3D
 
-var move_speed = 30.0
-var new_position
+@export var acceleration = 25.0
+@export var move_speed = 50.0
+@export var mouse_speed = 300.0
 
-func _process(delta):
+var velocity = Vector3.ZERO
+var look_angles = Vector2.ZERO
 
-	var direction = Vector3.ZERO
-
-	if Input.is_action_pressed("down"):
-		direction -= transform.basis.y.normalized()
-	if Input.is_action_pressed("up"):
-		direction += transform.basis.y.normalized()
-	if Input.is_action_pressed("left"):
-		direction -= transform.basis.x.normalized()
-	if Input.is_action_pressed("right"):
-		direction += transform.basis.x.normalized()
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
-	new_position = global_transform.origin + direction * move_speed * delta
-	global_transform.origin.x = new_position.x
-	global_transform.origin.z = new_position.z
+func _process(delta):
+	
+	look_angles.y = clamp(look_angles.y, PI / -2, PI / 2)
+	set_rotation(Vector3(look_angles.y, look_angles.x, 0))
+
+	var direction = update_direction()
+	
+	if direction.length_squared() > 0:
+		velocity += direction * acceleration * delta
+	
+	if velocity.length() > move_speed:
+		velocity = velocity.normalized() * move_speed
+		
+	translate(velocity * delta)
+
+func _input(event):
+	# Mouse Rotation
+	if event is InputEventMouseMotion:
+		look_angles -= event.relative / mouse_speed
+		
+func update_direction():
+	# Getting the direction to move the camera from input
+	
+	var direction = Vector3()
+
+	if Input.is_action_pressed("forward"):
+		direction += Vector3.FORWARD
+	if Input.is_action_pressed("backward"):
+		direction += Vector3.BACK
+	if Input.is_action_pressed("left"):
+		direction += Vector3.LEFT
+	if Input.is_action_pressed("right"):
+		direction += Vector3.RIGHT
+	if Input.is_action_pressed("up"):
+		direction += Vector3.UP
+	if Input.is_action_pressed("down"):
+		direction += Vector3.DOWN
+		
+	if direction == Vector3.ZERO:
+		velocity = Vector3.ZERO
+	
+	return direction.normalized()
+	
