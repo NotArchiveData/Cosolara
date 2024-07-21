70d35c603f373577d49b71186257d92b605dd1c7
Dhimaan prasad <89119442+NotArchiveData@users.noreply.github.com>
2024-07-22 01:32:36 +0530

    now theres a different material on hovering on houses!

--- House/gd/house_placer.gd
+++ House/gd/house_placer.gd
@@ -4,6 +4,8 @@ extends Node3D
 @onready var houses = %all_houses
 
 var house = preload("res://House/tscn/placed_house.tscn").instantiate()
+const HOVER = preload("res://House/colour/hover.tres")
+const GREEN = preload("res://House/colour/green.tres")
 
 func _on_area_3d_input_event(_camera, event, _position, _normal, _shape_idx):
 	if event is InputEventMouseButton:
@@ -16,8 +18,21 @@ func _on_area_3d_input_event(_camera, event, _position, _normal, _shape_idx):
 			
 			houses.hide()
 			global.housebuttonpressed = 0
-			
+
 func build_my_house():
 	house.global_transform = global_transform
 	placed_houses.add_child(house)
-	
+
+func mouse_entered():
+	colour_change(HOVER)
+
+func mouse_exited():
+	colour_change(GREEN)
+
+func colour_change(material):
+	for mesh in get_children():
+			if mesh.get_name() == "home" && mesh is MeshInstance3D:
+				mesh.material_override = material
+			
+			elif mesh.get_name() == "mound" && mesh is MeshInstance3D:
+				mesh.material_override = material
