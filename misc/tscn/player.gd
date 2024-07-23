1848e586bd7117994d0f044e768483547fc9b9fb
Dhimaan prasad <89119442+NotArchiveData@users.noreply.github.com>
2024-07-23 22:33:07 +0530

    Full minecraft creative mode-like flying done!

--- misc/tscn/player.gd
+++ misc/tscn/player.gd
@@ -3,10 +3,10 @@ extends CharacterBody3D
 @onready var head = $head
 @onready var headcam = $head/headcam
 
-var speed
+var speed = 10.0
 const WALK_SPEED = 10.0
 const SPRINT_SPEED = 20.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 7
 const SENSITIVITY = 0.01
 
 #bob variables
@@ -16,22 +16,19 @@ var t_bob = 0.0
 
 #fov variables
 const BASE_FOV = 90.0
-const FOV_CHANGE = 1.5
+const FOV_CHANGE = 0.5
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 9.8
+# gravity and flight
+var gravity = 20
+const NOT_FLYING = 20
+const FLYING = 0
 const FLY_UP = 10
 const FLY_DOWN = 10
-
 var flying = false
 
-# Time between presses to consider it a double press
-var double_press_time = 0.7
-
-# Timer to track the time since the last press
+# Double tap variables
+var double_press_time = 0.3
 var time_since_last_press = 0
-
-# Flag to indicate if the key was pressed once
 var single_press_detected = false
 
 func _ready():
@@ -41,36 +38,13 @@ func _physics_process(delta):
 	if single_press_detected:
 		time_since_last_press += delta
 	
-	var direction = main_movement()
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (head.transform.basis * Vector3(input_dir.x, 1, input_dir.y)).normalized()
 
-	# Add the gravity.
-	if not is_on_floor() && not flying:
-		velocity.y -= gravity * delta
-
-	elif is_on_floor() && flying:
-		gravity = 9.8
-		flying = false
+	apply_gravity(delta)
+	misc_movement()
+	apply_walking(delta, direction)
 	
-	if flying:
-		velocity.y = 0
-		
-	speed = WALK_SPEED
-	misc_movement(direction)
-	
-	# Walking
-	if is_on_floor():
-		if direction.x or direction.z:
-			velocity.x = direction.x * speed
-			velocity.z = direction.z * speed
-		else:
-			velocity.x = lerp(velocity.x, direction.x * speed, delta * 5.0)
-			velocity.z = lerp(velocity.z, direction.z * speed, delta * 5.0)
-	
-	else:
-		# adding jump inertia
-		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
-		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
-		
 	#headbob
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	headcam.transform.origin = headbob(t_bob)
@@ -81,15 +55,15 @@ func _physics_process(delta):
 	headcam.fov = lerp(headcam.fov, target_fov, delta * 8.0)
 
 	move_and_slide()
+	print("y velocity: ", velocity.y)
 
-# Head movement
 func _input(event):
 	if event is InputEventMouseMotion:
-		head.rotate_y(-event.relative.x * SENSITIVITY)
-		headcam.rotate_x(-event.relative.y * SENSITIVITY)
+		head.rotate_y(-event.relative.x * SENSITIVITY / 2)
+		headcam.rotate_x(-event.relative.y * SENSITIVITY / 2)
 		headcam.rotation.x = clamp(headcam.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
-func misc_movement(distance):
+func misc_movement():
 	# Handle sprint.
 	if Input.is_action_pressed("sprint"):
 		speed = SPRINT_SPEED
@@ -103,32 +77,29 @@ func misc_movement(distance):
 					# ONLY jump when on the ground
 					velocity.y = JUMP_VELOCITY
 				
-				elif not is_on_floor():
+				else:
 					# start flying if spacebar pressed again
-					gravity = 0
-					velocity.y = 0
+					gravity = FLYING
 					flying = true
-					print("does it keep coming here?")
-				
+					print("starting to fly")
+					
+			# gravity is not zero AND youre holding spacebar
 			else:
 				if is_on_floor():
 					# KEEP jumping if you hold spacebar
 					velocity.y = JUMP_VELOCITY
-				
-				elif gravity == 0:
-					# flying up
-					velocity.y = distance.y * FLY_UP
 		
 		elif gravity == 0:
 			# flying up
-			velocity.y = distance.y * FLY_UP
+			velocity.y += FLY_UP
 
 			# Check if the key is pressed
 			if Input.is_action_just_pressed("up"):
 				# If the key was pressed within the double_press_time, it's a double press
 				if single_press_detected and time_since_last_press <= double_press_time:
 					print("Double press detected")
-					gravity = 9.8
+					gravity = NOT_FLYING
+					velocity.y -= NOT_FLYING
 					flying = false
 					# Reset the timer and flag
 					time_since_last_press = 0
@@ -144,13 +115,40 @@ func misc_movement(distance):
 
 	# Handle flight ( downwards )
 	if Input.is_action_pressed("down"):
-		velocity.y -= FLY_DOWN
+		if not is_on_floor() && flying:
+			velocity.y -= FLY_DOWN
 
-func main_movement():
-	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 1, input_dir.y)).normalized()
+func apply_gravity(delta):
+	# Add the gravity.
+	if not is_on_floor() && not flying:
+		velocity.y -= gravity * delta
+
+	elif is_on_floor() && flying:
+		gravity = NOT_FLYING
+		flying = false
 	
-	return direction
+	elif not is_on_floor() && flying:
+		gravity = FLYING
+		velocity.y = 0
+
+func apply_walking(delta, direction):
+	# Walking
+	if is_on_floor():
+		if direction.x or direction.z:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x, direction.x * speed, delta * 5.0)
+			velocity.z = lerp(velocity.z, direction.z * speed, delta * 5.0)
+	
+	else:
+		# adding jump inertia
+		velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
+		velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+	
+	# Stops sprint if you stop walking
+	if abs(velocity.x) < 8.0 && abs(velocity.z) < 8.0:
+		speed = WALK_SPEED
 
 func headbob(time) -> Vector3:
 	var pos = Vector3.ZERO
